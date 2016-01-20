a7628c7c82ff5f118ec607a71dadde04e4464276
Marcin Krolik <marcin.krolik@intel.com>
2016-01-20 11:40:40 +0100

    wip

--- scripts/test.sh
+++ scripts/test.sh
@@ -35,6 +35,7 @@ if [[ $TEST_SUITE == "unit" ]]; then
 	go get golang.org/x/tools/cmd/vet
 	go get golang.org/x/tools/cmd/goimports
 	go get github.com/smartystreets/goconvey/convey
+	go get github.com/smartystreets/assertions
 	go get golang.org/x/tools/cmd/cover
 	
 	COVERALLS_TOKEN=t47LG6BQsfLwb9WxB56hXUezvwpED6D11
