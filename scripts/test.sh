2a5cc160e10dedbd2094cbb1cba883f3ed902c6f
Marcin Krolik <marcin.krolik@intel.com>
2016-01-20 12:38:41 +0100

    test

--- scripts/test.sh
+++ scripts/test.sh
@@ -101,6 +101,8 @@ if [[ $TEST_SUITE == "unit" ]]; then
 	#     done
 	# fi
 elif [[ $TEST_SUITE == "integration" ]]; then
+	go get github.com/smartystreets/goconvey/convey
+	go get github.com/smartystreets/assertions
 	cd scripts/docker/$INFLUX_VERSION; docker build -t intelsdi-x/influxdb:$INFLUX_VERSION .
         docker run -d --net=host -e PRE_CREATE_DB="test" intelsdi-x/influxdb:$INFLUX_VERSION	
 	cd $SNAP_PLUGIN_SOURCE
